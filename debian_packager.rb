324684b362a8dfc81d90458bd6df3e722c09caae
syntheticore <breidibreit@web.de>
2009-01-10 23:26:48 +0000

    Fixed package names

--- debian_packager.rb
+++ debian_packager.rb
@@ -16,23 +16,21 @@ begin
   cp_r 'data/schemas', "debian/usr/lib/ruby/site_ruby/#{v}/solid_matter/data/schemas"
   mkdir 'debian/DEBIAN'
   # create debian package description
-  control = '
-    Package: solid-matter
-    Version: 0.1
-    Section: graphics 
-    Priority: optional
-    Architecture: all
-    Essential: no
-    Pre-Depends: gtkglext 
-    Depends: ruby-gnome2 (>= 0.17), rmagick, ruby-dbus, ruby-narray
-    Recommends: luxrender
-    Installed-Size: 13746
-    Maintainer: Bjoern Breitgoff <breidibreit@web.de>
-    Description: Solid|matter
-                 A parametrical 3D-CAD application for
-                 the Gnome desktop enviroment.
-  '
-
+  control = 'Package: solid-matter
+Version: 0.1
+Section: graphics 
+Priority: optional
+Architecture: all
+Essential: no
+Pre-Depends: gtkglext 
+Depends: ruby-gnome2 (>= 0.17), librmagick-ruby, libdbus-ruby, libnarray-ruby
+Recommends: luxrender
+Installed-Size: 13746
+Maintainer: Bjoern Breitgoff <breidibreit@web.de>
+Description: Solid|matter
+             A parametrical 3D-CAD application for
+             the Gnome desktop enviroment.
+'
   File.open('debian/DEBIAN/control', 'w' ){|f| f << control }
   # update translation file
   #require 'post-setup.rb'
@@ -43,10 +41,11 @@ begin
       system("gconftool-2 --makefile-install-rule \'#{schema}\'")
     end
   '
-  File.open('debian/DEBIAN/postinst', 'w' ){|f| f << postinst }
+  File.open('debian/DEBIAN/postinst', 'w' ){|f| f << postinst ; f.chmod(0755) }
   postinst =
   # create debian package
   `dpkg -b debian solid-matter_0.1.0_all.deb`
 ensure
-  #rm_r 'debian'
+  rm_r 'debian'
 end
+
